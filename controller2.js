017a41f17a881b77d8c23611a605db7bcdb0cd4a
wjayne <wjayne@purdue.edu>
2015-10-17 01:01:11 -0400

    contoller changes

--- controller2.js
+++ controller2.js
@@ -26,14 +26,14 @@
 
         for(var j = 0; j<$scope.purchases.merchantsid.length ; j++)
         {
-          console.log(j) ;
+          //console.log(j) ;
           $http({
             method: 'GET',
             url: "http://api.reimaginebanking.com/merchants/"+$scope.purchases.merchantsid[j]+"?key=d764c1c8d9c91cf3b44b23287560aae7",
           }).then(function successCallback(response){
             //console.log($scope.purchases.merchantsid.length);
             //console.log(response.data.name) ;
-            console.log(j) ;
+            //console.log(j) ;
             $scope.purchases.mnames.push(response.data.name) ;
           },function errorCallback(response){
             console.log("fail") ;
@@ -45,46 +45,62 @@
 
   		$scope.clicky = function click()
   		{
-  			console.log($scope.out) ;
-  			console.log("hello") ;
+        var stuff = findTopThree()
+        //console.log(stuff) ;
+        //console.log(stuff.top1) ;
+        console.log(stuff.top1.num) ;
+  			console.log(stuff.top1.name) ;
+        //console.log(stuff.top.num) ;
+  			//console.log("hello") ;
   		}
 
-	}]) ;
-
-
-function findTopThree(){
-  var len = $scope.mnames.length ;
-  var hash = {} ;
-  top = {
-    num:0,
+      function findTopThree(){
+  var len = $scope.purchases.mnames.length ;
+  var hash = [] ;
+  top1 = {
+    num:1,
     name:""
   },
   top2={
-    num:0,
+    num:1,
     name:""
   },
   top3={
-    num:0,
+    num:1,
     name:""
   }
   for(var i = 0 ; i < len ; i++)
   {
-    hash[$scope.mnames[i]] += 1 ;
+    if(isNaN(hash[$scope.purchases.mnames[i]]))
+    {
+        hash[$scope.purchases.mnames[i]] = 1 ;
+    }
+    else
+    {
+        hash[$scope.purchases.mnames[i]]++ ;
+    }
+    //console.log($scope.purchases.mnames[i]) ;
+    //console.log(hash[$scope.purchases.mnames[i]])
+    //console.log(hash[$scope.purchases.mnames[i]]) ;
   }
 
   for(var k in hash)
   {
+    //console.log(k) ;
     if(hash.hasOwnProperty(k))
     {
-
+      //console.log("working") ;
+      //console.log(hash[k]) ;
       if(hash[k]>top3.num)
       {
         if(hash[k]>top2.num)
         {
-          if(hash[k]>top.num)
+          if(hash[k]>top1.num)
           {
-            top.num = hash[k] ;
-            top.name = k ;
+            top1.num = hash[k] ;
+            //console.log(top.num) ;
+            top1.name = k ;
+            //console.log(k) ;
           }
           else
           {
@@ -99,8 +115,18 @@ function findTopThree(){
         }
       }//end checking top 3
 
-    }
+    }//end for
+    //console.log(top.name + "-----hello") ;
+    //console.log(top1.num) ;
+    //console.log(top1.name) ;
+    var stuff = {top1, top2, top3} ;
+    return stuff ;
   }
-} 
+}
+
+	}]) ;
+
+
+ 
 
 })();
